97031cb0a30e29bae03d1d0990fcc61b62e773c5
Sean Anderson <sean@seananderson.ca>
2017-09-18 10:32:45 -0700

    Tweaks to simulation function

--- R/sim.R
+++ R/sim.R
@@ -1,5 +1,13 @@
 #' @export
-sim_sv <- function(time, sigma2_mu, sigma_obs, gp_scale, gp_sigma_sq, state0 = 1, sigma0 = 0) {
+# @examples
+# x <- sim_sv(1:50, sigma2_mu = rep(0.5, 49), sigma_obs = 0.001,
+#   gp_scale = 30, gp_sigma_sq = 0.2, state0 = 20)
+# plot(x$y[-c(1:2)], type = "p")
+# plot(x$states[-c(1:2)], type = "l")
+# plot(x$sigma[-c(1:2)], type = "l")
+
+sim_sv <- function(time, sigma2_mu, sigma_obs, gp_scale = 30, gp_sigma_sq = 0.2,
+  state0 = 0.1, sigma0 = 0) {
 
   maxt <- length(time)
   states <- vector(mode = "numeric", length = length(time))
@@ -8,7 +16,6 @@ sim_sv <- function(time, sigma2_mu, sigma_obs, gp_scale, gp_sigma_sq, state0 = 1
   states[1] <- state0
   sigma[1] <- 0
   pro_dev <- rnorm(length(time), 0, 1)
-
 
   for(i in 1:(maxt-1)) {
     for(j in 1:(maxt-1)) {
@@ -22,5 +29,6 @@ sim_sv <- function(time, sigma2_mu, sigma_obs, gp_scale, gp_sigma_sq, state0 = 1
     states[i] <- states[i-1] * exp((0 - sigma[i]*sigma[i]/2) * time[i] + sigma[i]* pro_dev[i-1]);
   }
   y <- rnorm(length(states), states, sigma_obs);
-  y
+
+  list(sigma = sigma, states = states, y = y, pro_dev = pro_dev)
 }
